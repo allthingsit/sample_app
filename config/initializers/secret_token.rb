fa74532793f6433ba2e1000bc05790c38a146712
allthingsit <info@allthingsit.co.ke>
2013-06-20 20:01:44 +0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8cf58f81b6d0e440406c888525de496dd7e5036e230619c06e06012d1cb27bad52b25a6d68aa3b56e27506ededb4b4894b67841087f663c88d8f5a3a4eaf2e23'
